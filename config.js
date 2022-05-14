e5260a973df45811c59f1821d94a0c660058a0ed
Kyle Schoonbee <kyles@bbd.co.za>
2022-05-14 11:56:11 +0200

    Docker config setup Environment setups

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+});
+
+module.exports = {
+    NODE_ENV : process.env.NODE_ENV || 'development',
+    PORT : process.env.PORT || 8080
+}
